2a86d9bfaf556863f83e1af616643b9e9d3eb0cb
egortens1a <120414001+egortens1a@users.noreply.github.com>
2024-09-25 23:52:45 +0400

    task4: task 4 was ready!!

---
+++ oopLab1/src/main/java/ru/ssau/tk/cucumber/oopLab1/Task4/myfirstpackage/MyFirstPackage.java
@@ -0,0 +1,26 @@
+package ru.ssau.tk.cucumber.oopLab1.Task4.myfirstpackage;
+
+public class MyFirstPackage{
+    private int boka;
+    private int zhoka;
+
+    public int getBoka() {
+        return boka;
+    }
+    public int getZhoka() {
+        return zhoka;
+    }
+    public void setBoka(int boka) {
+        this.boka = boka;
+    }
+    public void setZhoka(int zhoka) {
+        this.zhoka = zhoka;
+    }
+    public MyFirstPackage(int a, int b) {
+        this.boka = a;
+        this.zhoka = b;
+    }
+    public int activate(){
+        return (boka + zhoka);
+    }
+}

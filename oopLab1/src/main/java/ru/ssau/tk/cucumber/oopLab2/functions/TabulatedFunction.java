98362066f4d58e3d92f3757f9dcda8cacc8053b0
egortens1a <120414001+egortens1a@users.noreply.github.com>
2024-10-08 17:52:09 +0400

    task4

--- oopLab1/src/main/java/ru/ssau/tk/cucumber/oopLab2/functions/TabulatedFunction.java
+++ oopLab1/src/main/java/ru/ssau/tk/cucumber/oopLab2/functions/TabulatedFunction.java
@@ -4,6 +4,33 @@ public class TabulatedFunction {
     FunctionPoint[] values;
     double leftX;
     double rightX;
+
+    public double getLeftDomainBorder(){
+        return this.leftX;
+    }
+    public double getRightDomainBorder(){
+        return this.rightX;
+    }
+    double getFunctionValue(double x){
+        if (this.leftX < x && x < this.rightX){
+            for (int i = 0; i < this.values.length-1; ++i){
+                if (this.values[i].getX() > x){
+                    double x1 = this.values[i].getX();
+                    double x2 = this.values[i+1].getX();
+                    double y1 = this.values[i].getY();
+                    double y2 = this.values[i+1].getY();
+                    /*
+                    (x-x1)/(x2-x1) = (y-y1)/(y2-y1)
+                                <==>
+                    y = (x-x1)/(x2-x1)*(y2-y1)+y1
+                    */
+                    return (x-x1)/(x2-x1)*(y2-y1)+y1;
+                }
+            }
+        }
+        return Double.NaN;
+    }
+
     TabulatedFunction(double leftX, double rightX, int pointsCount){
         this.leftX = leftX;
         this.rightX = rightX;
